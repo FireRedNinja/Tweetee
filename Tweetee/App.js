112d141c0a4c2cb6f368f6f94cea6aa866f7407d
Noel <leonr13@hotmail.co.uk>
2018-02-27 11:49:43 +0000

    making buttons bigger and aligning them

--- Tweetee/App.js
+++ Tweetee/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { 
   StyleSheet,  
   View,
-  Image
+  Image,
+  ActivityIndicator,
+  // Button
 } from 'react-native';
 import {
   Container, 
   Content, 
   Header,
   H1, 
-  Button,
+  // Button,
   Card,
   Text,
   CardItem,
@@ -19,76 +21,69 @@ import {
   Input,
   Item,
   Label,
+  Button,
 } from 'native-base';
 import { TextInput } from 'react-native-gesture-handler';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import Roboto_medium from 'native-base/Fonts/Roboto_medium.ttf';
-import { Font, AppLoading } from "expo";
+
+
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { loading: true };
-  }  
-
-  
-
-  render() {
-  
-    return (
-<<<<<<< Updated upstream
-      <Container style={[styles.headerFix, styles.container]} >
-
-        <Container style={styles.logoHeader} >
-          <Image
-            source={require('./res/images/ic_launcher-web.png')}
-            style={{height: 80, width: 80}}
-          />
-          <H1 alignSelf='baseline'>Tweetee</H1>
-        </Container>
-
-        <Container style={styles.card}>
-          <Card >
-            
-            <CardItem style={styles.inputContainer}>
-              <Item floatingLabel >
-                <Label>Enter text</Label>
-                <Input multiline/>
-              </Item>
-            </CardItem>
-
-            <CardItem style={styles.buttonContainer}>
-
-              <Button iconLeft padding={20} style={{flex: 1}}>
-                <Icon name='camera-alt' />
-                {/* <Text aadjustsFontSizeToFit minimumFontScale={0.5}>Camera</Text> */}
-              </ Button>
-
-              <Button iconLeft Light padding={20} style={{flex: 1}}>
-                <Icon name='file-upload' />
-                {/* <Text adjustsFontSizeToFit minimumFontScale={0.5}>Upload</Text> */}
-              </ Button>
-
-              <Button iconLeft Light padding={20} style={{flex: -1}}>
-                <Icon name='translate' />
-                {/* <Text adjustsFontSizeToFit minimumFontScale={0.01}>Translate</Text> */}
-              </ Button>
-
-            </CardItem>
-          </Card>
-        </Container>
-      </Container>
-    );
   }
 
-  async componentWillMount() {
-    await Expo.Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-    });
-    this.setState({ loading: false });
+  render() {
+    if (!this.state.loading) {
+      return (
+        <Container>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </Container>
+      );
+    } else {
+      return (
+        <Container style={[styles.headerFix, styles.container]} >
+
+          <Container style={styles.logoHeader} >
+            <Image
+              source={require('./res/images/ic_launcher-web.png')}
+              style={{height: 80, width: 80}}
+            />
+            <H1 alignSelf='baseline'>Tweetee</H1>
+          </Container>
+
+          <Container style={styles.card}>
+            <Card >
+              
+              <CardItem style={styles.inputContainer}>
+                <Item floatingLabel >
+                  <Label>Enter text</Label>
+                  <Input multiline/>
+                </Item>
+              </CardItem>
+
+              <CardItem style={styles.buttonContainer}>
+
+                <Button  large primary style={styles.buttons}>
+                  <Icon name='camera-alt' size={24} color='white'/>
+                </ Button>
+
+                <Button large primary style={styles.buttons}>
+                  <Icon name='file-upload' size={24} color='white'/>
+                </ Button>
+
+                <Button large primary style={styles.buttons}>
+                  <Icon name='translate' size={24} color='white'/>
+                </ Button>
+
+              </CardItem>
+            </Card>
+          </Container>
+        </Container>
+      );
+    }
   }
 }
 
@@ -120,20 +115,16 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignSelf: 'flex-end',
     position: "absolute",
-    // bottom: 0,
+    bottom: 0,
     width: "100%",
   },
   inputContainer: {
-    flex: 1,
-    flexGrow: 1,
     alignSelf: 'flex-start',
   },
-<<<<<<< Updated upstream
-  Text: {
-    fontFamily: 'Roboto_medium',
-  } 
+  buttons: {
+    color: "#2293F7",
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+  }
 });
-=======
-
-});
->>>>>>> Stashed changes
