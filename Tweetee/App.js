e85ad067b2fdf04660a462423bf47fcc74200665
Edward Wood <2194801w@student.gla.ac.uk>
2018-03-06 12:24:01 +0000

    initial

--- Tweetee/App.js
+++ Tweetee/App.js
@@ -1,129 +1,58 @@
-import React from 'react';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
 import {
+  Platform,
   StyleSheet,
-  View,
-  Image,
-  ActivityIndicator,
-  // Button
-} from 'react-native';
-import {
-  Container,
-  Content,
-  Header,
-  H1,
-  Card,
   Text,
-  CardItem,
-  Left,
-  Body,
-  Right,
-  Input,
-  Item,
-  Label,
-  Button,
-} from 'native-base';
-import { TextInput } from 'react-native-gesture-handler';
-import { getStatusBarHeight } from 'react-native-status-bar-height';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import ShareMenu from 'react-native-share-menu';
+  View
+} from 'react-native';
 
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: true };
-  }
-
+type Props = {};
+export default class App extends Component<Props> {
   render() {
-    if (!this.state.loading) {
-      return (
-        <Container>
-          <ActivityIndicator size="large" color="#0000ff" />
-        </Container>
-      );
-    } else {
-      return (
-        <Container style={[styles.headerFix, styles.container]} >
-
-          <Container style={styles.logoHeader} >
-            <Image
-              source={require('./res/images/ic_launcher-web.png')}
-              style={{height: 80, width: 80}}
-            />
-            <H1 alignSelf='baseline'>Tweetee</H1>
-          </Container>
-
-          <Container style={styles.card}>
-            <Card >
-
-              <CardItem style={styles.inputContainer}>
-                <Item floatingLabel >
-                  <Label>Enter text</Label>
-                  <Input multiline/>
-                </Item>
-              </CardItem>
-
-              <CardItem style={styles.buttonContainer}>
-
-                <Button  large primary style={styles.buttons}>
-                  <Icon name='camera-alt' size={24} color='white'/>
-                </ Button>
-
-                <Button large primary style={styles.buttons}>
-                  <Icon name='file-upload' size={24} color='white'/>
-                </ Button>
-
-                <Button large primary style={styles.buttons}>
-                  <Icon name='translate' size={24} color='white'/>
-                </ Button>
-
-              </CardItem>
-            </Card>
-          </Container>
-        </Container>
-      );
-    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    margin: 32,
-  },
-  headerFix: {
-    marginTop: getStatusBarHeight(),
-    height: 54 + getStatusBarHeight(),
-  },
-  logoHeader: {
     flex: 1,
-    flexDirection: 'row',
-    padding: 16,
-    // paddingBottom: 24,
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  card: {
-    flex: 8,
-    alignSelf: 'stretch',
-    // margin: 16,
-    // padding: 16,
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
-  buttonContainer: {
-    flex: 0,
-    flexDirection: 'row',
-    alignSelf: 'flex-end',
-    position: "absolute",
-    bottom: 0,
-    width: "100%",
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
-  inputContainer: {
-    // alignSelf: 'flex-start',
-  },
-  buttons: {
-    flex: 1,
-    alignContent: "center",
-    justifyContent: "center",
-  }
 });
