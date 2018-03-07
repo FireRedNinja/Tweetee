e943c29be8b4629d3c8a5553d69ee9dda5f60a09
Rachel-Anne Hollywood <2195303H@student.gla.ac.uk>
2018-03-07 19:19:54 +0000

    Camera kinda sorta works

--- app/src/main/java/com/example/tweetee/MainActivity.java
+++ app/src/main/java/com/example/tweetee/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.tweetee;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button cam = findViewById(R.id.camera);
+
+        cam.setOnClickListener(this);
     }
-}
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this, CameraView.class));
+    }
+
+    }
+
+
