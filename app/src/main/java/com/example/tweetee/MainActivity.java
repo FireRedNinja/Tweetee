0b89e75cfc98671bf2ebea0468ec0efe16abd862
Edward Wood <2194801w@student.gla.ac.uk>
2018-03-07 19:08:45 +0000

    got sharing kinda working

--- app/src/main/java/com/example/tweetee/MainActivity.java
+++ app/src/main/java/com/example/tweetee/MainActivity.java
@@ -1,7 +1,19 @@
 package com.example.tweetee;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        EditText txtView = (EditText) findViewById(R.id.txt);
+        Intent receivedIntent = getIntent();
+        String receivedAction = receivedIntent.getAction();
+        String receivedType = receivedIntent.getType();
+
+        if(receivedAction.equals(Intent.ACTION_SEND)){
+            String receivedText = receivedIntent.getStringExtra(Intent.EXTRA_TEXT);
+            if (receivedText != null) {
+                txtView.setText(receivedText.substring(receivedText.lastIndexOf("https")));
+            }
+        }else if(receivedAction.equals(Intent.ACTION_MAIN)) {
+
+        }
+
+        }
+
+
 }
+
+
